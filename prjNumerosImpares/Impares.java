4884a15cf3d496f1435b724128b9a047b507f505
Raphael Chrystian <r.chrystian00@gmail.com>
2025-10-06 12:00:04 -0300

    Add files via upload

---
+++ prjNumerosImpares/Impares.java
@@ -0,0 +1,15 @@
+package prjNumerosImpares;
+
+public class Impares {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for (i=1;i<=1000;i+=2) {
+			System.out.print(" "+i);
+		}
+
+	}
+
+}
