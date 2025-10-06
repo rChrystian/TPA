1139d34c0e32b373c2d2c72f591d499bc756ebbd
Raphael Chrystian <r.chrystian00@gmail.com>
2025-10-06 12:01:09 -0300

    Add files via upload

---
+++ prjNumerosPares/NumerosPares.java
@@ -0,0 +1,16 @@
+package prjNumerosPares;
+
+public class NumerosPares {
+
+	public static void main(String[] args) {
+		
+		int i=2;
+		
+		do {
+			System.out.print(i+" ");
+			i = i+2;
+		} while (i<=500);
+		
+	}
+
+}
