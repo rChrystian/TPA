8b0914856f01f9d3d6ee45143cf6e0849bb1fab7
Raphael Chrystian <r.chrystian00@gmail.com>
2025-10-20 11:31:58 -0300

    Add files via upload

---
+++ Vetores/prjQuadrado/Quadrado.java
@@ -0,0 +1,31 @@
+package prjQuadrado;
+
+import java.util.Scanner;
+
+public class Quadrado {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		final int TAM=3;
+		int a[], b[], c[], i;
+		a = new int[TAM];
+		b = new int[TAM];
+			
+		for (i=0; i<TAM; i++) {
+			System.out.println("Digite o "+(i+1)+" valor do vetor A");
+			a[i] = in.nextInt(); 
+		}
+		
+		for (i=0; i<TAM; i++) {
+			b[i] = a[i]*a[i];
+		}
+		
+		System.out.print("\nB = ");
+		for (i=0; i<TAM; i++) {
+			System.out.print(b[i]+" ");
+		}
+	}
+
+}
